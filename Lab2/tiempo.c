eb9d7862c5180cc0d7ba881b3ac264e0fd07c6d5
Jose López <124711389+JPLopez23@users.noreply.github.com>
2026-02-13 23:33:27 -0600

    Update tiempo.c
    Removed comments from the tiempo.c file.

--- Lab2/tiempo.c
+++ Lab2/tiempo.c
@@ -6,25 +6,19 @@ int main() {
     clock_t inicio, fin;
     double tiempo_usado;
     
-    // Inicio del cronómetro
     inicio = clock();
     
-    // Primer ciclo de un millón de iteraciones
     for(int i = 0; i < 1000000; i++) {
     }
     
-    // Segundo ciclo de un millón de iteraciones
     for(int i = 0; i < 1000000; i++) {
     }
     
-    // Tercer ciclo de un millón de iteraciones
     for(int i = 0; i < 1000000; i++) {
     }
     
-    // Fin del cronómetro
     fin = clock();
     
-    // Calcular tiempo transcurrido
     tiempo_usado = (double)(fin - inicio) / CLOCKS_PER_SEC;
     
     printf("Tiempo transcurrido: %f segundos\n", tiempo_usado);
